427fcfc6bc059d3cd8c225f528a7a56ad9402c79
Radenko Vucanovic <radenkovuc@gmail.com>
2023-12-22 14:51:10 +0100

    update

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,5 +1,5 @@
 import {MongoClient} from "mongodb"
 
 export const connectToDatabase = async (): Promise<MongoClient> => {
-    return await MongoClient.connect("mongodb+srv://rade:raderade@mern.sfbppfv.mongodb.net/destinations?retryWrites=true&w=majority");
+    return await MongoClient.connect(process.env.MONGODB_URI || "");
 }
